58322036576b3febdd7b7b7c2013114fcdc92651
bellDev-code <sumaoo20@naver.com>
2021-04-28 14:40:48 +0900

    type practice

--- src/practice/coin.ts
+++ src/practice/coin.ts
@@ -1,9 +1,7 @@
 type Coin = (Bitcoin | Ethereum | Doge | CryptoChain) & {
   price: number;
-  kimp: number;
+  kimp: string;
 };
-
-// type CoinName = "Bitcoin" | "Ethereum" | "Doge" | "CryptoChain";
 
 type CoinInformation = {
   limit: number;
@@ -11,58 +9,110 @@ type CoinInformation = {
 
 type Bitcoin = {
   name: "Bitcoin";
-  Info: BitcoinInfo;
+  info: BitcoinInfo;
   unit: number;
+};
+
+type BlockProperty = {
+  blockTime: string;
+  protocol: string;
 };
 
 type BitcoinInfo = CoinInformation & {
   MarketCapitalization: number;
-  block: number;
+  block: BlockProperty;
 };
 
 type Ethereum = {
   name: "Ethereum";
-  Info: EthereumInfo;
+  info: EthereumInfo;
   unit: number;
 };
 
 type EthereumInfo = CoinInformation & {
   MarketCapitalization: number;
-  block: number;
-  protocol?: string;
+  block: BlockProperty;
 };
 
 type Doge = {
   name: "Doge";
-  Info: DogeInfo;
+  info: DogeInfo;
   unit: number;
 };
 
 type DogeInfo = CoinInformation & {
   MarketCapitalization: number;
-  block: number;
+  block: BlockProperty;
 };
 
 type CryptoChain = {
   name: "CryptoChain";
-  Info: CryptoInfo;
+  info: CryptoInfo;
   unit: number;
 };
 
-type CryptoInfo = {
+type CryptoInfo = CoinInformation & {
   MarketCapitalization: number;
-  block: number;
+  block: BlockProperty;
 };
 
 const coin01: Coin = {
   name: "Bitcoin",
   price: 5600000,
   unit: 1,
-  kimp: 7,
-  Info: {
+  kimp: "7%",
+  info: {
     MarketCapitalization: 1,
-    block: 2,
-    limit: 2000000,
+    block: {
+      blockTime: "10분",
+      protocol: "Pow",
+    },
+    limit: 21000000,
+  },
+};
+
+const coin02: Coin = {
+  name: "Ethereum",
+  price: 300000,
+  unit: 10,
+  kimp: "10%",
+  info: {
+    MarketCapitalization: 2,
+    block: {
+      blockTime: "12초",
+      protocol: "Pos",
+    },
+    limit: 100000000,
+  },
+};
+
+const coin03: Coin = {
+  name: "Doge",
+  price: 300,
+  unit: 100,
+  kimp: "13%",
+  info: {
+    MarketCapitalization: 5,
+    block: {
+      blockTime: "1분",
+      protocol: "Pow",
+    },
+    limit: 1000000000000000000,
+  },
+};
+
+const coin04: Coin = {
+  name: "CryptoChain",
+  price: 220,
+  unit: 1000,
+  kimp: "5%",
+  info: {
+    MarketCapitalization: 17,
+    block: {
+      blockTime: "20분",
+      protocol: "Pow",
+    },
+    limit: 3000000000,
   },
 };
 
@@ -72,4 +122,7 @@ const coinList = (coin: Coin) => {
 
 export const main = () => {
   coinList(coin01);
+  coinList(coin02);
+  coinList(coin03);
+  coinList(coin04);
 };
